e4b74b2adf788eeba0bdc5c1dcbe00ccbac949d9
dean <278245725@qq.com>
2017-02-18 19:21:51 +0800

    update

--- config/src/main/java/org/learning/eureka/Application.java
+++ config/src/main/java/org/learning/eureka/Application.java
@@ -1,17 +1,15 @@
-package org.learning.registry;
+package org.learning.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableConfigServer
 @EnableEurekaClient
-@EnableEurekaServer
+@EnableConfigServer
 public class Application {
 
 	public static void main(String[] args) {
